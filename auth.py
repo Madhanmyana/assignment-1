66856e4447c90119dd357c719fe0d6feba23becd
Madhanmyana <167597040+Madhanmyana@users.noreply.github.com>
2026-06-13 17:13:50 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -25,7 +25,7 @@ def create_access_token(data:dict):
 
 def verify_token(token: str):
     try:
-        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        payload = jwt.decode(token,SECRET_KEY,algorithm=[ALGORITHM])
         email = payload.get("sub")
 
         if email is None:
